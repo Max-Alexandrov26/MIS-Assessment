46afaf8e175700a2e411c3f6a965f2f5e36beaf2
Max-Alexandrov26 <152817559+Max-Alexandrov26@users.noreply.github.com>
2023-12-03 16:51:31 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ from model import get_predictions
 def main(input_repository):
     # Get predictions from YOLO
     get_predictions(input_repository)
-    #Select the latest run and generate footprints and overlaid images
+    #Select the latest run 
     number_of_runs = len(os.listdir('YOLO_predictions'))
     if number_of_runs !=1:
-        generate_footprint_and_overlay('YOLO_predictions\Latest_run'+str(number_of_runs),'YOLO_predictions\Latest_run'+str(number_of_runs)+'/labels')
+        path_to_yolo_images = 'YOLO_predictions\Latest_run'+str(number_of_runs)
+        path_to_yolo_labels = 'YOLO_predictions\Latest_run'+str(number_of_runs)+'/labels'
     else:
-         generate_footprint_and_overlay('YOLO_predictions\Latest_run','YOLO_predictions\Latest_run/labels')
+        path_to_yolo_images = 'YOLO_predictions\Latest_run'
+        path_to_yolo_labels = 'YOLO_predictions\Latest_run/labels'
+    #generate footprints and overlaid images
+    generate_footprint_and_overlay(path_to_yolo_images,path_to_yolo_labels)
     print("Footprint generated for images in: 'footprints'")
     print("Overlay generated for images in: 'overlays'")
     #Make the GEOJSON directory
@@ -21,9 +25,9 @@ def main(input_repository):
             shutil.rmtree(json_directory)
             os.makedirs(json_directory)
     # Process each txt file
-    for txt_file in os.listdir('YOLO_predictions/Latest_run/labels'):
+    for txt_file in os.listdir(path_to_yolo_labels):
         if txt_file.endswith(".txt"):
-            txt_file_path = os.path.join('YOLO_predictions/Latest_run/labels', txt_file)
+            txt_file_path = os.path.join(path_to_yolo_labels, txt_file)
             # Generate GeoJSON
             geojson_output_dir = json_directory+'/'+ txt_file + ".geojson"
             generate_geojson(txt_file_path, geojson_output_dir)
